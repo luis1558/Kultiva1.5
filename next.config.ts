df2e94258437b01ebcea5d1d204711c50e9a758c
Luis Castañeda Jimenez <lucaji1998@hotmail.com>
2026-01-27 22:18:06 -0500

    solucion bug restablecimiento de contraseña produccion

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,15 @@ const nextConfig: NextConfig = {
   /* config options here */
   images: {
     unoptimized: true, // Disable image optimization
-  }
+  },
+  async rewrites() {
+    return [
+      {
+        source: '/reset-password',
+        destination: '/reset-password',
+      },
+    ];
+  },
 };
 
 export default nextConfig;
